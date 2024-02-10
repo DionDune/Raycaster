3dbde96d9b808467e42805c10e77248fb3d3c5ba
DionDune <tomas.reynolds935@gmail.com>
2024-02-10 20:08:16 +0000

    Increased Cast resolution

--- RaycastGame/Settings.cs
+++ RaycastGame/Settings.cs
@@ -14,12 +14,12 @@ namespace RaycastGame
         public Color SkyColor = new Color(26, 117, 255);
         public Color RayPointColor = Color.White;
 
-        public const float CastRayJumpDistance = 0.5F;
-        public const int   CastRayCount = 920;
-        public const float CastRayWidth = 2;
+        public const float CastRayJumpDistance = 0.125F;
+        public const int   CastRayCount = 460;
+        public const float CastRayWidth = 4;
         public const float CastRayDistanceStart = 5;
         public const float CastRayDistanceRange = 500;
-        public const bool CastDistanceShadow = true;
+        public const bool  CastDistanceShadow = true;
         public const float DistanceShadowMult = 1.4F;
 
         public static bool RenderAllPoints = true;
